d6440b33772726618f4dd98409b9bc689fbd170a
miguel <hidalgoct837@gmail.com>
2022-05-17 22:54:18 -0400

    commiting for step1 adding comments

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -3,11 +3,15 @@ package com.example.springboot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Controller class to define endpoints for homepage
+ */
 @RestController
 public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
+		// returning a simple message for homepage
 		return "Hello World";
 	}
 
